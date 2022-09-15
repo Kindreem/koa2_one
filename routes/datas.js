b37d65457b35b1d2dda77cde330e0dbfffec5a2b
jimmy <Jimmy.jin@icloudengine.cnn>
2022-09-15 18:48:24 +0800

    feat: 返回body

--- routes/datas.js
+++ routes/datas.js
@@ -6,45 +6,50 @@ router.prefix('/datas')
 // 实现数据的添加
 router.post('/addData',async function(ctx,next){
   console.log(ctx.request.body, 111111)
+  try {
+    await Promise.all(ctx.request.body.map((el) => {
+      return new Promise(async (resolve, reject) => {
+        const obj = await data.find({name: el.name})
 
-  Promise.all(ctx.request.body.map(async (el) => {
+        if (obj && obj.length) { // 编辑
+          data.findOneAndUpdate({
+            name: el.name
+          }, { $set: {
+            count: obj[0].count + 1,
+            time: Math.round((obj[0].count * obj[0].time + el.time) / (obj[0].count + 1))
+          }}, {}, (err, res) => {
+            if (err) {
+              console.log("Error:" + err);
+              reject(err)
+            } else {
+              console.log("Res:" + res);
+              resolve(res)
+            }
+          })
 
-    const obj = await data.find({name: el.name})
+        } else { // 新增
+          let result = new data({  // 给data数据对象中添加数据
+            time: el.time, // 设置数据
+            name: el.name,
+            count: 1
+          })
 
-    if (obj && obj.length) { // 编辑
-      data.findOneAndUpdate({
-        name: el.name
-      }, { $set: {
-        count: obj[0].count + 1,
-        time: Math.round((obj[0].count * obj[0].time + el.time) / (obj[0].count + 1))
-      }}, {}, (err, res) => {
-        if (err) {
-          console.log("Error:" + err);
-        } else {
-          console.log("Res:" + res);
+          result.save((err, res) => { // 把设置好数据提交
+            if (err) {
+              console.log("Error:" + err);
+              reject(err)
+            } else {
+              console.log("Res:" + res);
+              resolve(res)
+            }
+          })
         }
       })
-
-    } else { // 新增
-      let result = new data({  // 给data数据对象中添加数据
-        time: el.time, // 设置数据
-        name: el.name,
-        count: 1
-      })
-
-      result.save((err, res) => { // 把设置好数据提交
-        if (err) {
-          console.log("Error:" + err);
-        } else {
-          console.log("Res:" + res);
-        }
-      })
-    }
-  })).then((r)=> {
-    ctx.body = {
-      r
-    }
-  })
+    }))
+    ctx.body = '上传成功'
+  } catch (error) {
+    ctx.body = error
+  }
 });
 
 module.exports = router
